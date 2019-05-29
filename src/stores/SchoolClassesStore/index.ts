1dffd793c4be39e2a7ce17366a8293c18cdc79d0
Igor Tironi <tironiigor@gmail.com>
2019-05-29 19:53:14 -0300

    added loading and added commentaries

--- src/stores/SchoolClassesStore/index.ts
+++ src/stores/SchoolClassesStore/index.ts
@@ -18,29 +18,7 @@ export default class SchoolClassesStore extends VariableChangeHandler {
 	@observable public selectedSchoolClassProfessores: api.Professor[] = [];
 
 	@observable public commentary: string = "";
-	@observable public commentaries = [
-		{
-			user: {
-				name: "Arthur Fernandes",
-				avatar: null as {url: string} | null,
-			},
-			commentary: "Excelente aula, finalmente entendi o assunto!",
-		},
-		{
-			user: {
-				name: "Filipe Arlindo",
-				avatar: null as {url: string} | null,
-			},
-			commentary: "Excelente aula, mas a prova é barril",
-		},
-		{
-			user: {
-				name: "Thiago Mariano",
-				avatar: null as {url: string} | null,
-			},
-			commentary: "Barril, ném vá",
-		},
-	];
+	@observable public commentaries: api.Commentary[] = [];
 
 	@action
 	public selectSchoolClass = async (schoolClassId: string) => {
@@ -61,7 +39,11 @@ export default class SchoolClassesStore extends VariableChangeHandler {
 
 	@action
 	public getProfessoresForSelectSchoolClass = async () => {
-		if (this.loading || !this.selectedSchoolClass) {
+		if (this.loading) {
+			return;
+		}
+
+		if (!this.selectedSchoolClass) {
 			return;
 		}
 
@@ -77,18 +59,58 @@ export default class SchoolClassesStore extends VariableChangeHandler {
 	}
 
 	@action
-	public sendCommentary = async () => {
-		this.commentaries = [
-			{
-				user: {
-					name: "Arthur Fernandes",
-					avatar: null as {url: string} | null,
-				},
-				commentary: this.commentary,
-			},
-			...this.commentaries,
-		];
+	public getCommentariesForSchoolClass = async () => {
+		if (this.loading) {
+            return;
+		}
 
-		this.commentary = "";
+		if (!this.selectedSchoolClass) {
+			return;
+		}
+
+		this.loading = true;
+
+        try {
+			this.commentaries = await api.getCommentariesForSchoolClass(this.selectedSchoolClass.id);
+        } catch (e) {
+            uiStore.openErrorSnackbar(e);
+        } finally {
+            this.loading = false;
+        }
+	}
+
+	@action
+	public sendCommentary = async () => {
+		if (this.loading) {
+            return;
+		}
+
+		if (!this.selectedSchoolClass) {
+			return;
+		}
+
+		this.loading = true;
+
+        try {
+			const newCommentary = await api.createCommentary({
+				text: this.commentary,
+				professorId: null,
+				schoolClassId: this.selectedSchoolClass.id,
+			});
+
+			this.commentaries = [newCommentary, ...this.commentaries];
+        } catch (e) {
+            uiStore.openErrorSnackbar(e);
+        } finally {
+            this.loading = false;
+			this.commentary = "";
+        }
+	}
+
+	@action
+	public clear = async () => {
+		this.selectedSchoolClass = null;
+		this.selectedSchoolClassProfessores = [];
+		this.commentaries = [];
 	}
 }
